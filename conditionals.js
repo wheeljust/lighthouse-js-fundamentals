22e0e1b4a73e4f91db7cb177fd7689e183331612
Justin Wheeler <justinwwheeler@gmail.com>
2021-06-11 19:56:30 +0000

    added more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -7,5 +7,16 @@ if (temperature < 0) {
 } else {
   console.log("Short sleeves are fine.");
 }
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
 
 console.log("Now you're ready to go outside!");
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
