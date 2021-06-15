aef7f7a3f5a7715222f8df8134178fa7c3d43564
Justin Wheeler <justinwwheeler@gmail.com>
2021-06-15 16:26:02 +0000

    seach arrays for values and return the last index of given number

---
+++ last-index.js
@@ -0,0 +1,17 @@
+function lastIndexOf(array,num){
+  let indexed = -1;
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === num){
+      indexed = i;
+    }
+  }
+  return indexed;
+}
+
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
